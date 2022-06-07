5c4b75789476a978a13f4f0d66802857b24dc6dc
akdedeimtisal <akdedeimtisall@gmail.com>
2022-06-07 16:31:33 +0300

    fetch api url add

--- frontend/estore-clone-flowers-frontend/src/Fetch.js
+++ frontend/estore-clone-flowers-frontend/src/Fetch.js
@@ -4,16 +4,16 @@ function Flowers() {
     const [flowers, setFlowers] = useState([]);
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users') //burası backendden gelen API'ye göre değişecek şu an öylesine bir data koydum.
+        fetch('http://localhost:5008/api/Flower') //burası backendden gelen API'ye göre değişecek şu an öylesine bir data koydum.
         .then((res) => res.json())
         .then((flowers) =>setFlowers(flowers));
 
     }, [])
 
 
-    return <div> <h1>Users</h1>
+    return <div> <h1>Flowers</h1>
     {
-        flowers.map(flower => <div key={flower.id}>{flower.name}
+        flowers.map(flower => <div key={flower.ID}>{flower.name} {flower.price}
          </div>)
     }
     
