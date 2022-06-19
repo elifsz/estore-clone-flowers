aee6dc4943dfa69e0db2b8435c17011b5e09721a
akdedeimtisal <akdedeimtisall@gmail.com>
2022-06-19 18:38:20 +0300

    rose fixed

--- frontend/api-js/index.js
+++ frontend/api-js/index.js
@@ -1,8 +1,10 @@
-
-const uriCategory = 'https://localhost:7225/api/Categories'
-const uriflowersByCategoryNo = 'https://localhost:7225/api/Flowers/filter?categoryNo=2'
-const uriflowersByCategoryNoWedding = 'https://localhost:7225/api/Flowers/filter?categoryNo=1'
-const uriFlower = "https://localhost:7225/api/Flowers/8d179247-897f-45d6-95e3-1786f74462b3"
+const uriCategory = "https://localhost:7225/api/Categories";
+const uriflowersByCategoryNo =
+  "https://localhost:7225/api/Flowers/filter?categoryNo=2";
+const uriflowersByCategoryNoWedding =
+  "https://localhost:7225/api/Flowers/filter?categoryNo=1";
+const uriFlower =
+  "https://localhost:7225/api/Flowers/8d179247-897f-45d6-95e3-1786f74462b3";
 /*let categories = [
   {
     catergoryNo: 1,
@@ -23,53 +25,47 @@ const uriFlower = "https://localhost:7225/api/Flowers/8d179247-897f-45d6-95e3-17
     categoryDescription: 'Birthday Flowers',
   },
 ]*/
-let categories = []
-let flowersByCategoryNo = []
-let flowersByCategoryNoWedding = []
+let categories = [];
+let flowersByCategoryNo = [];
+let flowersByCategoryNoWedding = [];
 
 function getFlower() {
   fetch(uriFlower)
-  .then((response) => response.json())
-<<<<<<< Updated upstream
-  .then((data) => _displayItemsforCategoryDetail(data))
-  .catch((error) => console.error('Unable to get flowers.', error))
-  console.log(data)
-=======
-  .then((data) => _displayFlower(data))
-  .catch((error) => console.error('Unable to get books.', error))
-  
->>>>>>> Stashed changes
+    .then((response) => response.json())
+    .then((data) => _displayFlower(data))
+    .catch((error) => console.error("Unable to get books.", error));
 }
 
 function getflowersByCategoryNo() {
   fetch(uriflowersByCategoryNo)
     .then((response) => response.json())
     .then((data) => _displayItemsforCategoryDetail(data))
-    .catch((error) => console.error('Unable to get items.', error))
-  console.log(flowersByCategoryNo)
+    .catch((error) => console.error("Unable to get books.", error));
+  console.log(flowersByCategoryNo);
 }
 
 function getflowersByCategoryNoWedding() {
   fetch(uriflowersByCategoryNoWedding)
     .then((response) => response.json())
     .then((data) => _displayItemsforCategoryDetail(data))
-    .catch((error) => console.error('Unable to get items.', error))
-  console.log(flowersByCategoryNoWedding)
+    .catch((error) => console.error("Unable to get books.", error));
+  console.log(flowersByCategoryNoWedding);
 }
 
 function getCategory() {
   fetch(uriCategory)
     .then((response) => response.json())
     .then((data) => _displayItemsforCategory(data))
-    .catch((error) => console.error('Unable to get items.', error))
-  console.log(categories)
+    .catch((error) => console.error("Unable to get books.", error));
+  console.log(categories);
 }
 
 function _displayFlower(data) {
   let flowerInfo = data;
-  let flowerHtml =  `
+  let flowerHtml = `
   <div class="col-lg-25 col-sm-5">
   <div class="box_main" >
+
       <div class="electronic_img">
       <img src="../images/gul.jpg" />
       </div>
@@ -78,27 +74,27 @@ function _displayFlower(data) {
       </div>
 </div>
 </div>
-   <div class="col-lg-25 col-sm-5">
-         
-   <h4 class="shirt_text">${flowerInfo.flowerName}</h4>
-   <p class="price_text"> <br/>
-   ${flowerInfo.flowerDescription} <br/> <br/>
-   ${flowerInfo.price} $ <br/> <br/>
- Delivery Time:  ${flowerInfo.deliveryTime} 
-
-   </p>
-   </div>
+</br> 
+<div class="col-lg-35 col-sm-5">
+<h4 class="shirt_text"> </br>  </br>  ${flowerInfo.flowerName}</h4> </br> </br>
+<p class="price_text">
+${flowerInfo.flowerDescription} <br/> <br/>
+Price: ${flowerInfo.price} $ <br/> <br/>
+Delivery Time:  ${flowerInfo.deliveryTime}  <br/> <br/>
+</p>
 </div>
-`
 
-document
-.querySelector('#flower')
-.insertAdjacentHTML('afterbegin', flowerHtml)
+</div>
+`;
+
+  document
+    .querySelector("#flower")
+    .insertAdjacentHTML("afterbegin", flowerHtml);
 }
 
 function _displayItemsforCategory(data) {
-  categories = data
-  let categoryHtml =''
+  categories = data;
+  let categoryHtml = "";
   for (let i = 0; i < categories.length; i++) {
     categoryHtml += `
                 <div class="col-lg-4 col-sm-4">
@@ -117,18 +113,16 @@ function _displayItemsforCategory(data) {
             </div>
             </div>
             </div>
-            `
+            `;
   }
   document
-    .querySelector('#category_list')
-    .insertAdjacentHTML('afterbegin', categoryHtml)
+    .querySelector("#category_list")
+    .insertAdjacentHTML("afterbegin", categoryHtml);
 }
 
-
-
 function _displayItemsforCategoryDetail(data) {
-  flowersByCategoryNo = data
-  let categoryHtml =''
+  flowersByCategoryNo = data;
+  let categoryHtml = "";
   for (let i = 0; i < flowersByCategoryNo.length; i++) {
     categoryHtml += `
                 <div class="col-lg-4 col-sm-4">
@@ -148,16 +142,16 @@ function _displayItemsforCategoryDetail(data) {
             </div>
             </div>
             </div>
-            `
+            `;
   }
   document
-    .querySelector('#categoryDetail')
-    .insertAdjacentHTML('afterbegin', categoryHtml)
+    .querySelector("#categoryDetail")
+    .insertAdjacentHTML("afterbegin", categoryHtml);
 }
 
 function _displayItemsforCategoryDetailWedding(data) {
-  flowersByCategoryNoWedding = data
-  let categoryHtml =''
+  flowersByCategoryNoWedding = data;
+  let categoryHtml = "";
   for (let i = 0; i < flowersByCategoryNoWedding.length; i++) {
     categoryHtml += `
                 <div class="col-lg-4 col-sm-4">
@@ -177,45 +171,40 @@ function _displayItemsforCategoryDetailWedding(data) {
             </div>
             </div>
             </div>
-            `
+            `;
   }
   document
-    .querySelector('#categoryDetail')
-    .insertAdjacentHTML('afterbegin', categoryHtml)
+    .querySelector("#categoryDetail")
+    .insertAdjacentHTML("afterbegin", categoryHtml);
 }
 
-
-
 function getFlowerFromCategory(categoryName) {
-  console.log("flew"+books)
+  console.log("flew" + books);
   for (let i = 0; i < books.length; i++) {
-    if (
-      getCategoryNameFromCategoryNo(books[i].catergoryNo) === categoryName
-    ) {
-      console.log('bura' + books[i].flowerName)
+    if (getCategoryNameFromCategoryNo(books[i].catergoryNo) === categoryName) {
+      console.log("bura" + books[i].flowerName);
     }
   }
 }
 
-function getSearchFlower(searchInput){
+function getSearchFlower(searchInput) {
   var uriSearch = "https://localhost:7225/api/Flowers/search?flowerNameSearch";
   fetch(`${uriSearch}=${searchInput}`)
-  .then((response) => response.json())
-  .then((data) => _display(data))
-  .catch((error) => console.error('Unable to get flowers.', error))
+    .then((response) => response.json())
+    .then((data) => _display(data))
+    .catch((error) => console.error("Unable to get flowers.", error));
 }
 
 function searchFlower() {
   console.log("fffff");
   var searchInput = $("#searchId").val();
   getSearchFlower(searchInput);
- 
 }
 
 function _display(data) {
   console.log(data);
   var flowerUrl = `${data[0].flowerName}`;
-  flowerUrl = flowerUrl.toLowerCase(); 
+  flowerUrl = flowerUrl.toLowerCase();
   var url = `http://localhost:3000/flowers/${flowerUrl}`;
   window.location.assign(url);
 }
